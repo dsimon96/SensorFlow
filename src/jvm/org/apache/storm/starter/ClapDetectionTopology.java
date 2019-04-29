3c8515bb153022e42628f08faf3f10aa6b155df1
zeleena <zkearney@andrew.cmu.edu>
2019-04-29 17:25:03 -0400

    RabbitMQ Sink and Spout in between 2 data processing bolts to allow processing on Edge or Cloud.

--- src/jvm/org/apache/storm/starter/ClapDetectionTopology.java
+++ src/jvm/org/apache/storm/starter/ClapDetectionTopology.java
@@ -16,7 +16,6 @@ import org.apache.storm.tuple.Fields;
 import org.apache.storm.tuple.Tuple;
 import org.apache.storm.tuple.Values;
 
-import java.nio.ByteBuffer;
 import java.util.LinkedList;
 import java.util.Map;
 import java.util.Queue;
@@ -25,6 +24,7 @@ public class ClapDetectionTopology {
 
     private static String rabbitmqExchange = "sf.topic";
     private static String rabbitmqSpoutRoutingKey = "edge.info";
+    private static String rabbitmqMidboltRoutingKey = "midbolt.info";
     private static String rabbitmqSinkRoutingKey = "cloud.info";
     private static String rabbitmqHost = "localhost";
     private static int rabbitmqPort = 5672;
@@ -87,8 +87,12 @@ public class ClapDetectionTopology {
         @Override
         public void execute(Tuple tuple) {
             try {
-                float volumeAverage = tuple.getFloat(0).floatValue();
-                float currentVolume = tuple.getFloat(1).floatValue();
+                //float volumeAverage = tuple.getFloat(0).floatValue();
+                //float currentVolume = tuple.getFloat(1).floatValue();
+                String midboltMsg = tuple.getString(0);
+                String[] splitMsg = midboltMsg.split(";", 2);
+                float volumeAverage = Float.parseFloat(splitMsg[0]);
+                float currentVolume = Float.parseFloat(splitMsg[1]);
 
                 System.out.println("at clap2, received " + volumeAverage + " and " + currentVolume);
 
@@ -99,7 +103,6 @@ public class ClapDetectionTopology {
                 } else {
                     _collector.emit(new Values("nothing"));
                 }
-
                 _collector.ack(tuple);
             }
             catch (Exception e) {
@@ -114,87 +117,85 @@ public class ClapDetectionTopology {
         }
     }
 
-    // From int to byte array converter from
-    // https://stackoverflow.com/questions/1936857/convert-integer-into-byte-array-java
-    private static byte[] intToBytes( final int i ) {
-        ByteBuffer bb = ByteBuffer.allocate(4);
-        bb.putInt(i);
-        return bb.array();
+    public static IRichSpout CreateRabbitSpout(String spoutRoutingKey) {
+        // RabbitMQ as Spout
+        Scheme scheme = new SensorFlowMessageScheme();
+        Declarator declarator = new SensorFlowStormDeclarator(rabbitmqExchange, "", spoutRoutingKey);
+        return new RabbitMQSpout(scheme, declarator);
+    }
+
+    public static ConsumerConfig CreateRabbitSpoutConfig(String Vhost) {
+        ConnectionConfig connectionConfig = new ConnectionConfig(rabbitmqHost, rabbitmqPort, rabbitmqUsername, rabbitmqPassword, Vhost, 10);
+        return new ConsumerConfigBuilder().connection(connectionConfig)
+                .queue("") // No queue name
+                .prefetch(200)
+                .requeueOnFail()
+                .build();
+    }
+
+    public static ProducerConfig CreateRabbitSinkConfig(String Vhost, String sinkRoutingKey) {
+        ConnectionConfig sinkConnectionConfig = new ConnectionConfig(rabbitmqHost, rabbitmqPort, rabbitmqUsername, rabbitmqPassword, Vhost, 10);
+        return new ProducerConfigBuilder()
+                .connection(sinkConnectionConfig)
+                .contentEncoding("UTF-8")
+                .contentType("application/json")
+                .exchange(rabbitmqExchange)
+                .routingKey(sinkRoutingKey)
+                .build();
     }
 
     public static void main(String[] args) throws Exception {
         TopologyBuilder builder = new TopologyBuilder();
 
-        // RabbitMQ as Spout
-        Scheme scheme = new SensorFlowMessageScheme();
-        Declarator declarator = new SensorFlowStormDeclarator(rabbitmqExchange, "", rabbitmqSpoutRoutingKey);
-        IRichSpout spout = new RabbitMQSpout(scheme, declarator);
-
-        ConnectionConfig connectionConfig = new ConnectionConfig(rabbitmqHost, rabbitmqPort, rabbitmqUsername, rabbitmqPassword, rabbitmqEdgeVhost, 10);
-        ConsumerConfig spoutConfig = new ConsumerConfigBuilder().connection(connectionConfig)
-                .queue("") // No queue name
-                .prefetch(200)
-                .requeueOnFail()
-                .build();
-
-        builder.setSpout("rabbit1", spout, 1)
-            .addConfigurations(spoutConfig.asMap())
+        // Rabbit Spout as Sensor Input
+        IRichSpout spout1 = CreateRabbitSpout(rabbitmqSpoutRoutingKey);
+        ConsumerConfig spout1Config = CreateRabbitSpoutConfig(rabbitmqEdgeVhost);
+        builder.setSpout("rabbit-spout1", spout1, 1)
+            .addConfigurations(spout1Config.asMap())
             .setMaxSpoutPending(200);
 
-        builder.setBolt("clap1", new ClapDetection1Bolt(), 1).shuffleGrouping("rabbit1");
-        builder.setBolt("clap2", new ClapDetection2Bolt(), 1).shuffleGrouping("clap1");
+        builder.setBolt("clap1", new ClapDetection1Bolt(), 1).shuffleGrouping("rabbit-spout1");
 
-        Config conf = new Config();
-        conf.setDebug(true);
-
-        /* RabbitMQ as Sink, dynamic */
-        /*TupleToMessage sinkScheme = new TupleToMessage() {
+        // Midbolt Sink and Spout
+        TupleToMessage sink1Scheme = new TupleToMessageNonDynamic() {
             @Override
-            public byte[] extractBody(Tuple input) { return input.getStringByField("word").getBytes(); }
-
-            @Override
-            public String determineExchangeName(Tuple input) { return input.getStringByField(rabbitmqExchange); }
-
-            @Override
-            public String determineRoutingKey(Tuple input) { return input.getStringByField(rabbitmqSinkRoutingKey); }
-
-            //@Override
-            //public Map<String, Object> specifiyHeaders(Tuple input) { return new HashMap<String, Object>(); }
-
-            @Override
-            public String specifyContentType(Tuple input) { return "application/json"; }
-
-            @Override
-            public String specifyContentEncoding(Tuple input) { return "UTF-8"; }
-
-            @Override
-            public boolean specifyMessagePersistence(Tuple input) { return false; }
+            public byte[] extractBody(Tuple input) {
+                String volAvgStr = Float.toString(input.getFloatByField("vol_avg"));
+                String currVolStr = Float.toString(input.getFloatByField("curr_vol"));
+                return (volAvgStr + ";" + currVolStr).getBytes();
+            }
         };
-        ConnectionConfig sinkConnectionConfig = new ConnectionConfig(rabbitmqHost, rabbitmqPort, rabbitmqUsername, rabbitmqPassword, rabbitmqCloudVhost, 10);
-        ProducerConfig sinkConfig = new ProducerConfigBuilder().connection(sinkConnectionConfig).build();
-        builder.setBolt("rabbitmq-sink", new RabbitMQBolt(sinkScheme))
-                .addConfigurations(sinkConfig.asMap())
-                .shuffleGrouping("frame1");
-        */
+        ProducerConfig sink1Config = CreateRabbitSinkConfig(rabbitmqEdgeVhost, rabbitmqMidboltRoutingKey);
 
-        /* RabbitMQ as Sink, message attributes are non-dynamic */
-        TupleToMessage sinkScheme = new TupleToMessageNonDynamic() {
+        builder.setBolt("rabbit-sink1", new RabbitMQBolt(sink1Scheme))
+                .addConfigurations(sink1Config.asMap())
+                .shuffleGrouping("clap1");
+
+        IRichSpout spout2 = CreateRabbitSpout(rabbitmqMidboltRoutingKey);
+        ConsumerConfig spout2Config = CreateRabbitSpoutConfig(rabbitmqEdgeVhost);
+        builder.setSpout("rabbit-spout2", spout2, 1)
+                .addConfigurations(spout2Config.asMap())
+                .setMaxSpoutPending(200);
+
+
+        builder.setBolt("clap2", new ClapDetection2Bolt(), 1).shuffleGrouping("rabbit-spout2");
+
+        // RabbitMQ as Sink, message attributes are non-dynamic
+        TupleToMessage sink2Scheme = new TupleToMessageNonDynamic() {
           @Override
           public byte[] extractBody(Tuple input) { return input.getStringByField("detection").getBytes(); }
         };
 
-        ConnectionConfig sinkConnectionConfig = new ConnectionConfig(rabbitmqHost, rabbitmqPort, rabbitmqUsername, rabbitmqPassword, rabbitmqCloudVhost, 10);
-        ProducerConfig sinkConfig = new ProducerConfigBuilder()
-                .connection(sinkConnectionConfig)
-                .contentEncoding("UTF-8")
-                .contentType("application/json")
-                .exchange(rabbitmqExchange)
-                .routingKey(rabbitmqSinkRoutingKey)
-                .build();
+        // Rabbit Sink as Application Output
+        ProducerConfig sink2Config = CreateRabbitSinkConfig(rabbitmqCloudVhost, rabbitmqSinkRoutingKey);
 
-        builder.setBolt("rabbitmq-sink", new RabbitMQBolt(sinkScheme))
-                .addConfigurations(sinkConfig.asMap())
+        builder.setBolt("rabbit-sink2", new RabbitMQBolt(sink2Scheme))
+                .addConfigurations(sink2Config.asMap())
                 .shuffleGrouping("clap2");
+
+
+        Config conf = new Config();
+        conf.setDebug(true);
 
         if (args != null && args.length > 0) {
             conf.setNumWorkers(3);
